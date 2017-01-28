6e661b1d413982afcb46e68430af61856bf6a625
davidmrtz <d.19mar@gmail.com>
2017-01-28 18:28:17 +0100

    server secretarias

--- server.js
+++ server.js
@@ -115,6 +115,377 @@ app.get("/euskara", function (req, res){
 	
 });
 
+app.get("/castArriaga", function (req, res){
+	
+  var now = new Date();
+  var hour = now.getHours();
+  var day = now.getDay();
+  var minutes = now.getMinutes();
+  
+  if(day == 1 || day == 2 ||day == 3 ||day == 4){
+  
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30 || hour >= 16 && hour < 20){
+		var respuesta = [
+			{
+				text: "¡Ahora está abierta!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Está cerrada ahora mismo...",
+			}
+		];
+    }
+  
+  } else if (day == 5) {  	  	
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30 || hour >= 16 && hour < 18){
+		var respuesta = [
+			{
+				text: "¡Ahora está abierta!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Está cerrada ahora mismo...",
+			}
+		];
+    }
+  } else if (day == 6 || day == 7) {
+		var respuesta = [
+			{
+				text: "Está cerrada ahora mismo...",
+			}
+		];
+  }
+	
+	// Devolver el objeto en formato JSON
+            res.json(respuesta);
+	
+});
+
+app.get("/euskArriaga", function (req, res){
+	
+  var now = new Date();
+  var hour = now.getHours();
+  var day = now.getDay();
+  var minutes = now.getMinutes();
+  
+  if(day == 1 || day == 2 ||day == 3 ||day == 4){
+  
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30 || hour >= 16 && hour < 20){
+		var respuesta = [
+			{
+				text: "Irekita dago oraintxe bertan!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Oraintxe bertan itxita dago...",
+			}
+		];
+    }
+  
+  } else if (day == 5) {  	  	
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30 || hour >= 16 && hour < 18){
+		var respuesta = [
+			{
+				text: "Irekita dago oraintxe bertan!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Oraintxe bertan itxita dago...",
+			}
+		];
+    }
+  } else if (day == 6 || day == 7) {
+		var respuesta = [
+			{
+				text: "Oraintxe bertan itxita dago...",
+			}
+		];
+  }
+	
+	// Devolver el objeto en formato JSON
+            res.json(respuesta);
+	
+});
+
+app.get("/castJO", function (req, res){
+	
+  var now = new Date();
+  var hour = now.getHours();
+  var day = now.getDay();
+  var minutes = now.getMinutes();
+  
+  if(day == 1 || day == 2 ||day == 3 ||day == 4){
+  
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30 || hour >= 16 && hour < 20){
+		var respuesta = [
+			{
+				text: "¡Ahora está abierta!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Está cerrada ahora mismo...",
+			}
+		];
+    }
+  
+  } else if (day == 5) {  	  	
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30 || hour >= 16 && hour < 19){
+		var respuesta = [
+			{
+				text: "¡Ahora está abierta!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Está cerrada ahora mismo...",
+			}
+		];
+    }
+  } else if (day == 6 || day == 7) {
+		var respuesta = [
+			{
+				text: "Está cerrada ahora mismo...",
+			}
+		];
+  }
+	
+	// Devolver el objeto en formato JSON
+            res.json(respuesta);
+	
+});
+
+app.get("/euskJO", function (req, res){
+	
+  var now = new Date();
+  var hour = now.getHours();
+  var day = now.getDay();
+  var minutes = now.getMinutes();
+  
+  if(day == 1 || day == 2 ||day == 3 ||day == 4){
+  
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30 || hour >= 16 && hour < 20){
+		var respuesta = [
+			{
+				text: "Irekita dago oraintxe bertan!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Oraintxe bertan itxita dago...",
+			}
+		];
+    }
+  
+  } else if (day == 5) {  	  	
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30 || hour >= 16 && hour < 19){
+		var respuesta = [
+			{
+				text: "Irekita dago oraintxe bertan!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Oraintxe bertan itxita dago...",
+			}
+		];
+    }
+  } else if (day == 6 || day == 7) {
+		var respuesta = [
+			{
+				text: "Oraintxe bertan itxita dago...",
+			}
+		];
+  }
+	
+	// Devolver el objeto en formato JSON
+            res.json(respuesta);
+	
+});
+
+app.get("/castMendi", function (req, res){
+	
+  var now = new Date();
+  var hour = now.getHours();
+  var day = now.getDay();
+  var minutes = now.getMinutes();
+  
+  if(day == 1 || day == 2 ||day == 3 ||day == 4 || day == 5){
+  
+    if(hour >= 8 && hour < 12 || hour == 12 && minutes < 45){
+		var respuesta = [
+			{
+				text: "¡Ahora está abierta!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Está cerrada ahora mismo...",
+			}
+		];
+    }
+  
+  } else if (day == 6 || day == 7) {
+		var respuesta = [
+			{
+				text: "Está cerrada ahora mismo...",
+			}
+		];
+  }
+	
+	// Devolver el objeto en formato JSON
+            res.json(respuesta);
+	
+});
+
+app.get("/eusMendi", function (req, res){
+	
+  var now = new Date();
+  var hour = now.getHours();
+  var day = now.getDay();
+  var minutes = now.getMinutes();
+  
+  if(day == 1 || day == 2 ||day == 3 ||day == 4 || day == 5){
+  
+    if(hour >= 8 && hour < 12 || hour == 12 && minutes < 45){
+		var respuesta = [
+			{
+				text: "Irekita dago oraintxe bertan!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Oraintxe bertan itxita dago...",
+			}
+		];
+    }
+  
+  } else if (day == 6 || day == 7) {
+		var respuesta = [
+			{
+				text: "Oraintxe bertan itxita dago...",
+			}
+		];
+  }
+	
+	// Devolver el objeto en formato JSON
+            res.json(respuesta);
+	
+});
+
+app.get("/castMoliNC", function (req, res){
+	
+  var now = new Date();
+  var hour = now.getHours();
+  var day = now.getDay();
+  var minutes = now.getMinutes();
+  
+  if(day == 1 || day == 2 ||day == 3 ||day == 4){
+  
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30 || hour >= 16 && hour < 20){
+		var respuesta = [
+			{
+				text: "¡Ahora está abierta!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Está cerrada ahora mismo...",
+			}
+		];
+    }
+  
+  } else if (day == 5) {  	  	
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30){
+		var respuesta = [
+			{
+				text: "¡Ahora está abierta!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Está cerrada ahora mismo...",
+			}
+		];
+    }
+  } else if (day == 6 || day == 7) {
+		var respuesta = [
+			{
+				text: "Está cerrada ahora mismo...",
+			}
+		];
+  }
+	
+	// Devolver el objeto en formato JSON
+            res.json(respuesta);
+	
+});
+
+app.get("/eusMoliNC", function (req, res){
+	
+  var now = new Date();
+  var hour = now.getHours();
+  var day = now.getDay();
+  var minutes = now.getMinutes();
+  
+  if(day == 1 || day == 2 ||day == 3 ||day == 4){
+  
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30 || hour >= 16 && hour < 20){
+		var respuesta = [
+			{
+				text: "Irekita dago oraintxe bertan!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Oraintxe bertan itxita dago...",
+			}
+		];
+    }
+  
+  } else if (day == 5) {  	  	
+    if(hour >= 8 && hour <= 13 || hour == 13 && minutes < 30){
+		var respuesta = [
+			{
+				text: "Irekita dago oraintxe bertan!",
+			}
+		];
+    } else {
+		var respuesta = [
+			{
+				text: "Oraintxe bertan itxita dago...",
+			}
+		];
+    }
+  } else if (day == 6 || day == 7) {
+		var respuesta = [
+			{
+				text: "Oraintxe bertan itxita dago...",
+			}
+		];
+  }
+	
+	// Devolver el objeto en formato JSON
+            res.json(respuesta);
+	
+});
 
 // Arrancar el servidor y dejarlo a la espera
 app.listen(PORT, function () {
